ac0ee9803303c83a9870ae12a1f4d2e40d13f065
PxychoCaxon <89336177+PxychoCaxon@users.noreply.github.com>
2026-02-04 01:42:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const commands = {
         if (args === 'skills.txt') return config.skills;
         if (args === 'README.txt') return "available commands: whoami, ls, cat, note, fastfetch, clear";
         if (args === 'aliases.txt') return "cax, cakeson, cake, psy, pxffy, batman";
-        if (args === 'secrets.txt') return "1. my password is very obvious\n2. the more i live the less my tolerance gets\n3. i maintain a mad scientist's diary and a dream journal irl\n4. i once rm rf-ed a system\n5. i love pixel art\n6. im...";
+        if (args === 'secrets.txt') return "1. my password is very obvious\n2. the more i live the less my tolerance gets\n3. i maintain a mad scientist's diary and a dream journal irl\n4. i once rm rf-ed a system\n5. i like pixel art";
         return `File not found: ${args}`;
     },
 
@@ -184,5 +184,6 @@ function initMatrix() {
     });
     setInterval(draw, 35);
 }
+
 
 
